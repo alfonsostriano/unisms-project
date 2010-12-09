dc1195de200401f2c8de8a38c371f993a6ec02e1
gerardo.caruso@usi.ch <gerardo.caruso@usi.ch@c57a3ec6-deb4-b149-bb9c-d9b31d8007e6>
2010-12-09 17:26:09 +0000

    Open/close for adding contact. Add contact without refresh.

--- database_add.php
+++ database_add.php
@@ -5,7 +5,7 @@ require 'connect.php';
 session_name('tzLogin');
 // Starting the session
 session_start();
-$id = $_SESSION['id'];
+$owner = $_SESSION['id'];
 
 
 	// Retrieve data from Query String
@@ -16,14 +16,22 @@ $phone = $_GET['phone'];
 $names = mysql_real_escape_string($names);
 $phone = mysql_real_escape_string($phone);
 
-
 	//build query
-$query = "INSERT INTO `unisms`.`contacts` (`id`, `names`, `phone`, `owner`) VALUES (NULL, '".$names."', '".$phone."', '".$id."')";
+$query = "INSERT INTO `unisms`.`contacts` (`id`, `names`, `phone`, `owner`) VALUES (NULL, '".$names."', '".$phone."', '".$owner."')";
 	//Execute query
 $qry_result = mysql_query($query) or die(mysql_error());
 
-  echo "<div id='contact'>";
-  echo "<h4 id='contact_name' onclick='getNumber(".$id.")'>" . $name . "</h4>";
-  echo "<img id='delete_button' src='img/delete.png' onclick='remove_contact(".$id.")'/>";   
-  echo "</div>";
+// Retrieve all the data from the "example" table
+$list_retrieve = mysql_query("SELECT * FROM contacts WHERE owner = '$owner'")
+or die(mysql_error());
+while ($list = mysql_fetch_array($list_retrieve)) {
+$name = $list['names'];
+$id = $list['id'];
+echo "<div id='contact'>";
+                echo "<h4 id='contact_name' onclick='getNumber(".$id.")'>" . $name . "</h4>";
+                echo "<img id='delete_button' src='img/delete.png' onclick='remove_contact(".$id.")'/>";
+                //echo "<h4 onclick='remove_contact(".$id.")'> REMOVE </h4>";
+                echo "</div>";
+}
+include 'google.php';
 ?>
