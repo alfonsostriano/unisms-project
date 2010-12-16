9010c996019245072d264ab2faf8dbd5662db92b
M6R9productions@gmail.com <M6R9productions@gmail.com@c57a3ec6-deb4-b149-bb9c-d9b31d8007e6>
2010-12-16 10:45:03 +0000

    fading jquery notifications

--- login_panel/js/slide.js
+++ login_panel/js/slide.js
@@ -2,18 +2,28 @@ $(document).ready(function() {
 	
 	// Expand Panel
 	$("#open").click(function(){
-		$("div#panel").slideDown("slow");
+		$("div#panel").slideDown("fast");
 	
 	});	
 	
 	// Collapse Panel
 	$("#close").click(function(){
-		$("div#panel").slideUp("slow");	
+		$("div#panel").slideUp("fast");	
 	});		
 	
 	// Switch buttons from "Log In | Register" to "Close Panel" on click
 	$("#toggle a").click(function () {
 		$("#toggle a").toggle();
 	});		
-		
+	
+	$('.notification span').click(function() {
+	   jQuery(this).parents('.notification').fadeOut(800);
+	});
+	
+	
+	$('.notification').hover(function() {
+    jQuery(this).css('cursor','pointer');
+  }, function() {
+    jQuery(this).css('cursor','auto');
+  }); // Close notifications
 });
