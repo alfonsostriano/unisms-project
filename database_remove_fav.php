d2ff9b2ba3c8ff05f1e97d2f169d0c571d3ea2dd
M6R9productions@gmail.com <M6R9productions@gmail.com@c57a3ec6-deb4-b149-bb9c-d9b31d8007e6>
2010-12-17 07:30:07 +0000

    Can remove favourite

---
+++ database_remove_fav.php
@@ -0,0 +1,24 @@
+<?php
+require 'connect.php';
+
+
+session_name('tzLogin');
+// Starting the session
+session_start();
+$owner = $_SESSION['id'];
+
+
+// Retrieve data from Query String
+$nameid = $_GET['nameid'];
+  // Escape User Input to help prevent SQL Injection
+$nameid = mysql_real_escape_string($nameid);
+
+  //build query
+$query = "UPDATE `unisms`.`contacts` SET `favourite`=0 WHERE `contacts`.`id` = '$nameid'";
+
+  //Execute query
+
+mysql_query($query) or die(mysql_error());
+
+require_once('address_book_gen.php');
+?>
